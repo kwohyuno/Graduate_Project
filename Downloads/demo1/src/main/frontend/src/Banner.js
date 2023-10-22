70f9f3b9d24bb79c6431a5114c2f506c5c189706
Hyun Oh <you@example.com>
2023-10-22 16:48:36 +0900

    project complete

--- Downloads/demo1/src/main/frontend/src/Banner.js
+++ Downloads/demo1/src/main/frontend/src/Banner.js
@@ -1,18 +1,42 @@
 import awsbanner from './assets/awslogo_.png'
+    import fptbanner from './assets/fptlogo.png'
+    import bitbanner from './assets/bitcamp.png'
+    import cukbanner from './assets/cuklogo.png'
+    import mcsabanner from './assets/mcsalogo.jpeg'
 
-function Banner(props){
+    function Banner(props){
 
     return(
         <div className="banner">
             배너
             <div className="banner-box">
-                <div className="banner-box-one">
-                    <a href="https://aws.amazon.com/ko/">
+                <a href="https://aws.amazon.com/ko/">
+                    <div className="banner-box-one">
                         <img src={awsbanner} className="banner-box-one-awsbanner" />
-                    </a>
-                </div>
-                <div className="banner-box-two"></div>
-                <div className="banner-box-three"></div>
+                    </div>
+                </a>
+                <a href="https://www.fpt-software.kr">
+                    <div className="banner-box-two">
+                        <img src={fptbanner} className="banner-box-two-fptbanner"/>
+                    </div>
+                </a>
+                <a href="https://www.bitcamp.co.kr">
+                    <div className="banner-box-three">
+                        <img src={bitbanner} className="banner-box-three-bitbanner"/>
+                    </div>
+                </a>
+
+                <a href="http://mcsa.or.kr">
+                    <div className="banner-box-four">
+                        <img src={mcsabanner} className="banner-box-four-mcsabanner"/>
+                    </div>
+                </a>
+
+                <a href="https://www.cuk.edu/">
+                    <div className="banner-box-five">
+                        <img src={cukbanner} className="banner-box-five-cukbanner"/>
+                    </div>
+                </a>
             </div>
         </div>
     )
--- Downloads/demo1/src/main/frontend/src/Project.js
+++ Downloads/demo1/src/main/frontend/src/Project.js
@@ -4,7 +4,40 @@ function Project(props){
 
     return(
         <div className="project">
-            프로젝트
+            <div className="project-one">
+                <p>
+                    <h2>프로젝트 1. Devster v2</h2>
+                    &nbsp;Online community for preliminary software engineer
+                    <ul>
+                        <li>Migrated Front-End components from JSP to React.js in order to upgrade user experience and code agility<br/></li>
+                        <li>Adopted Docker to enhance CICD process and to prepare for potential scaling of server<br/></li>
+                        <li>Empowered security by developing JWT, and prevented HTML intrusion by inserting related code<br/></li>
+                    </ul>
+                </p>
+            </div>
+            <div className="project-two">
+                <p>
+                    <h2>프로젝트 2. Devster v1</h2>
+                    &nbsp;Online community for preliminary software engineer
+                    <ul>
+                        <li>Designed DB(MySQL) and bulletin board with MyBatis, Spring Boot, JSP, jQuery, and Ajax</li>
+                        <li>Inserted company information on bulletin board via Python</li>
+                        <li>Deployed servers via Naver Cloud, Jenkins, and Tomcat</li>
+                        <li>Improved design with CSS operation for a better user experience</li>
+                    </ul>
+                </p>
+            </div>
+            <div className="project-three">
+                <p>
+                    <h2>프로젝트3. Just Say</h2>
+                    &nbsp;English Practice Web Application
+                    <ul>
+                        <li>Developed web services where people can practice foreign languages, by using GPT 3.5 and Google TTS, as well as Fast API, Git Action, React.js, and AWS</li>
+                        <li>Tuned prompt engineering to make GPT reply appropriate responses</li>
+                        <li>Ranked 16th out of 226 teams (made it to the finals for the event)</li>
+                    </ul>
+                </p>
+            </div>
         </div>
     )
 

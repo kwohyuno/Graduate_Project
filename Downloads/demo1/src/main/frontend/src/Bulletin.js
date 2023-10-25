913a934efae0b5a823e3c166a7aaf1abd1ffa012
Hyun Oh <you@example.com>
2023-10-25 17:39:20 +0900

    read bulletinlist(sql)

--- Downloads/demo1/src/main/frontend/src/Bulletin.js
+++ Downloads/demo1/src/main/frontend/src/Bulletin.js
@@ -1,6 +1,24 @@
 import {Route, Link, NavLink, useNavigate} from 'react-router-dom';
+import axios from "axios";
+import {useEffect, useState} from "react";
+
 function Bulletin(props){
     const navi = useNavigate();
+
+    const [BoardList, setBoardList] = useState([]);
+
+    useEffect(()=> {
+        axios
+            .get("/api/boardlist")
+            .then((response) => {
+                console.log(response.data);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    },[]);
+
+
     return(
         <div>
 
@@ -8,7 +26,13 @@ function Bulletin(props){
             <div className="bulletin">
                 게시판
                 <div><button className="bulletin-write" onClick={()=>{navi('/bulletinform');}}>글쓰기</button></div>
+
+                <div className="bulletin-list">
+
+                </div>
             </div>
+
+
         </div>
     )
 

cc2085af3a5dc54b8991ba244bae96ef8314ae7a
Hyun Oh <you@example.com>
2023-10-25 18:59:39 +0900

    CRUD _delete

--- Downloads/demo1/src/main/frontend/src/Bulletin.js
+++ Downloads/demo1/src/main/frontend/src/Bulletin.js
@@ -6,12 +6,30 @@ function Bulletin(props){
     const navi = useNavigate();
 
     const [BoardList, setBoardList] = useState([]);
+    const refreshPage = () => {
+        window.location.reload();
+    }
+
+    const deletePost = (idx) => {
+        // console.log(idx);
+        // alert(idx);
+        axios
+            .delete(`/api/boarddelete/${idx}`)
+            .then((res)=>{
+                console.log("삭제완료");
+                refreshPage();
+            })
+            .catch((error)=>{
+                console.log(error)
+            })
+    }
 
     useEffect(()=> {
         axios
             .get("/api/boardlist")
             .then((response) => {
                 console.log(response.data);
+                setBoardList(response.data)
             })
             .catch((error) => {
                 console.log(error);
@@ -22,16 +40,24 @@ function Bulletin(props){
     return(
         <div>
 
-
             <div className="bulletin">
                 게시판
                 <div><button className="bulletin-write" onClick={()=>{navi('/bulletinform');}}>글쓰기</button></div>
 
                 <div className="bulletin-list">
-
+                    {BoardList.map((board)=>{
+                        return(
+                            <div className="bulletin-list-eachpost">
+                                이름 : {board.name} <br/><br/>
+                                작성시간 : {board.time} <br/><br/>
+                                내용 : {board.content} <br/><br/>
+                                <button className="bulletin-list-eachpost-delete" onClick={()=>deletePost(board.idx)}>삭제</button>
+                                <button className="bulletin-list-eachpost-update">수정</button>
+                            </div>
+                        );
+                    })}
                 </div>
             </div>
-
 
         </div>
     )
